22bc6609c3b76f2136acb53405c83f6c8ecec9f3
Ruben <rubenernestovalle@gmail.com>
2024-10-11 09:07:09 -0600

    Factorial de un numero

---
+++ factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿
+  
+
+
+long Factorial(int numero)
+{
+  if (numero == 0 || numero == 1)
+  {
+    return 1;
+ }
+
+  return numero * Factorial(numero - 1);
+}
+
+Console.WriteLine("----------------------------Programa para calcular el fatorial de un numero--------------------------------");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Ingrese un numero: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+
+  
+Console.WriteLine($"El factorial del numero {numero} es {Factorial(numero)}");
