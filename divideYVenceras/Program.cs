281848db0c33bc0a626f506287581e5d6ff67784
Ruben <rubenernestovalle@gmail.com>
2024-10-11 01:47:50 -0600

    Añadir menú para seleccionar el cálculo del factorial

--- divideYVenceras/Program.cs
+++ divideYVenceras/Program.cs
@@ -1,3 +1,41 @@
-﻿Console.WriteLine("-----------------------------Menu-------------------------");
+﻿long Factorial(int numero)
+{
+    if (numero == 0 || numero == 1)
+    {
+        return 1;
+    }
+
+    return numero * Factorial(numero - 1);
+}
+
+Console.WriteLine("-----------------------------Menu-------------------------");
 Console.WriteLine();
 
+Console.WriteLine("1.Encontrar el Factorial de un numero");
+Console.WriteLine();
+Console.WriteLine("2.Algoritmo de ordenamiento QuickSort");
+Console.WriteLine();
+Console.WriteLine("3.Pasar un numero decimal a binario");
+Console.WriteLine();
+
+bool bandera = true;
+int option = 0;
+
+while (bandera)
+{
+    switch (option)
+    {
+        case 1: Console.WriteLine("----------------------------Programa para calcular el fatorial de un numero--------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Ingrese un numero: ");
+
+            int numero = int.Parse(Console.ReadLine());
+
+
+  
+            Console.WriteLine($"El factorial del numero {numero} es {Factorial(numero)}");
+            break;
+        
+    }
+}
